f78082cf71f5506409fa99bbd0d9b58cf78cbc0c
Parziphal <asterixvader@gmail.com>
2014-07-10 04:29:07 -0500

    Fixed 2 for 3 args.

--- src/Rails/Toolbox/AppRenamer.php
+++ src/Rails/Toolbox/AppRenamer.php
@@ -29,7 +29,7 @@ class AppRenamer
         
         foreach ($files as $file) {
             $contents = file_get_contents($file);
-            $contents = str_replace('NewApp', $appName);
+            $contents = str_replace('NewApp', $appName, $contents);
             file_put_contents($contents, $file);
         }
         
