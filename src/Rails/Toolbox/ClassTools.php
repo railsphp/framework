15871bf5ccc695f34a001a3769e04f4d28faa321
Parziphal <asterixvader@gmail.com>
2014-07-16 22:09:12 -0500

    Added method.

--- src/Rails/Toolbox/ClassTools.php
+++ src/Rails/Toolbox/ClassTools.php
@@ -18,4 +18,20 @@ class ClassTools
             return $class;
         }
     }
+    
+    /**
+     * @param string|object $class
+     */
+    public static function getNamespace($class)
+    {
+        if (is_object($class)) {
+            $class = get_class($class);
+        }
+        
+        if (is_int($pos = strrpos($class, '\\'))) {
+            return substr($class, 0, $pos);
+        } else {
+            return '';
+        }
+    }
 }
