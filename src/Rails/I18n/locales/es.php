69a21400d2e548b9f3fec15c4b2f075f4bff4f79
Parziphal <asterixvader@gmail.com>
2014-08-02 22:05:57 -0500

    Added spanish translations

---
+++ src/Rails/I18n/locales/es.php
@@ -0,0 +1,27 @@
+<?php
+return ['es' => [
+    'errors' => [
+        'messages' => [
+            "inclusion" => "no está incluido en la lista",
+            "exclusion" => "es reservado",
+            "invalid" => "es inválido",
+            "confirmation" => "no es igual a la confirmación",
+            "accepted" => "debe ser aceptado",
+            "empty" => "no puede quedar vacío",
+            "blank" => "no puede quedar en blanco",
+            "too_long" => "es muy largo (%{count} caracteres máximo)",
+            "too_short" => "es muy corto (%{count} caracteres mínimo)",
+            "wrong_length" => "tiene una longitud errónea (debería ser %{count} caracteres)",
+            "not_a_number" => "no es un número",
+            "not_an_integer" => "debe ser un número entero",
+            "greater_than" => "debe ser mayor a %{count}",
+            "greater_than_or_equal_to" => "debe ser mayor o igual a %{count}",
+            "equal_to" => "debe ser igual a %{count}",
+            "less_than" => "debe ser menor a %{count}",
+            "less_than_or_equal_to" => "debe ser menor o igual a %{count}",
+            "odd" => "debe ser impar",
+            "even" => "debe ser par",
+            "uniqueness" => "debe ser único"
+        ]
+    ]
+]];
